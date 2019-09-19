23cb246348cf2fa3bdd9a1a8d4a80c2d285a998f
TosterMocy <tostermocy@gmail.com>
2019-09-19 23:33:28 +0200

    a i d

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 20f;
+
+    private Vector2 directionOfMovement;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+    
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            directionOfMovement = Vector2.left;
+            rb.AddForce(directionOfMovement*movementSpeed);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            directionOfMovement = Vector2.right;
+            rb.AddForce(directionOfMovement*movementSpeed);
+        }
+    }
+}
