892725d3dc4f600b57969beaa926b0d6b9a96a7a
TosterMocy <tostermocy@gmail.com>
2019-10-30 23:07:21 +0100

    gowno

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,42 +5,77 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 10f;
-    public float jumpSpeed = 20f;
+    public float movementSpeed = 5f;
+    public float jumpHeight = 4f;
 
     private Vector2 directionOfMovement;
     private float horizontalMovement;
+    private float verticalMovement;
+    private bool isGrounded = false;
+    private float playerScaleX;
 
-    private Rigidbody2D rb;
+    private Rigidbody rb;
 
     private void Awake()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody>();
+        playerScaleX = transform.localScale.x;
     }
 
     private void Update()
     {
-        horizontalMovement = Input.GetAxis("Horizontal");
-        directionOfMovement = new Vector2(horizontalMovement,0);
         
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        
+        //jumping
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            }
+        }
+        
+        
+        horizontalMovement = Input.GetAxis("Horizontal");
+        
+        directionOfMovement = new Vector2(horizontalMovement,verticalMovement) ;
     }
 
     private void FixedUpdate()
     {
         MoveCharacter(directionOfMovement);
-        
     }
 
     void MoveCharacter(Vector2 dir)
     {
         rb.MovePosition((Vector2)transform.position + (Time.deltaTime * movementSpeed * dir));
-        
+        if (dir.x < 0) playerScaleX = -1;
+        if (dir.x > 0) playerScaleX = 1;
+
+        transform.localScale = new Vector3(playerScaleX, 1f, 1f);
+
     }
 
     void Jump()
     {
-        Debug.Log("skacze");
-        rb.AddForce(transform.up * jumpSpeed);
+        //rb.AddForce(Vector2.up * jumpHeight, ForceMode.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        isGrounded = false;
+
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            //Debug.Log("collision with ground");
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        //Debug.Log("is floating true");
+        isGrounded = false;
     }
 }
---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+   private float xAxis = 0;
+   private float yAxis = 0;
+}
