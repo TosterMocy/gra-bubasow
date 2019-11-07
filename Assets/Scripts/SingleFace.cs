4c58ac6d24ea8aaa46fac9ef104540ffe104a247
TosterMocy <tostermocy@gmail.com>
2019-11-07 22:05:27 +0100

    start of mesh deform

---
+++ Assets/Scripts/SingleFace.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleFace : MonoBehaviour
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+    
+}
