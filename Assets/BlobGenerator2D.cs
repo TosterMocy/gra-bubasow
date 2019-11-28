438160a589fce601f4a5687aa41d407d00a81989
Necr0o0 <radekklaser@gmail.com>
2019-11-28 14:03:43 +0100

    zabawa

---
+++ Assets/BlobGenerator2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobGenerator2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject blob;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
