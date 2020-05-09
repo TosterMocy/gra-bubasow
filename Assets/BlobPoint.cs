98610c484d3c180af206ff7d6af977eb7bc054ae
Toster <tostermocy@gmail.com>
2020-05-10 01:47:53 +0200

    added particle system (blob splash) and 2 scripts (Player and BlobPoint)

---
+++ Assets/BlobPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobPoint : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (transform.parent.GetComponent<Player>())
+        {
+            if (other.transform.CompareTag("Ground"))
+            {
+                transform.parent.GetComponent<Player>().ChildCollisionDetection(transform);
+                
+                Debug.Log("BlobPoint pozycja" + transform.position);
+                //Debug.Log("kolizja dziecka");
+            }
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public ParticleSystem splashParticles;
+
+    private void Start()
+    {
+        //splashParticles.transform.position = transform.position;
+    }
+    
+    
+
+
+    public void ChildCollisionDetection(Transform blobPoint)
+    {
+        Debug.Log("pozycja particli 1: " + splashParticles.transform.position);
+        Debug.Log("pozycja blobow w rodzicu " + blobPoint.position);
+        
+        splashParticles.transform.position = blobPoint.position;
+        
+        Debug.Log("nowa pozycja particli: " + splashParticles.transform.position);
+        splashParticles.Play();
+        
+        //Debug.Log("kolizja rodzica");
+    }
+}
